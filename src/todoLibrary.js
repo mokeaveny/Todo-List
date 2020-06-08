f373b3c1f121b9a4163886fd956495e553209315
mokeaveny <michaelkeaveny0@gmail.com>
2020-06-08 18:55:31 +0100

    Fixed a bug when the user clicks show list before adding any todo items

--- src/todoLibrary.js
+++ src/todoLibrary.js
@@ -11,23 +11,33 @@ export function renderTodoList() {
 	const todoItemContainer = document.createElement("div");
 	todoItemContainer.id = "current-container";
 	todoItemContainer.classList.add("todo-item-container");
+
+	if (todoList.length == 0) {
+		const displayEmpty = document.createElement("h1");
+		displayEmpty.textContent = "Hello";
+		todoItemContainer.appendChild(displayEmpty);
+	}
 	
-	todoList.forEach( todoItem => {
-		const todoTitle = document.createElement("h1");
-		const todoDesc = document.createElement("p");
-		const todoDueDate = document.createElement("p");
-		const todoPriority = document.createElement("p");
+	else {
+		todoList.forEach( todoItem => {
+			const todoTitle = document.createElement("h1");
+			const todoDesc = document.createElement("p");
+			const todoDueDate = document.createElement("p");
+			const todoPriority = document.createElement("p");
 
-		todoTitle.textContent = todoItem.title;
-		todoDesc.textContent = todoItem.description;
-		todoDueDate.textContent = todoItem.due_date;
-		todoPriority.textContent = todoItem.priority;
+			todoTitle.textContent = todoItem.title;
+			todoDesc.textContent = todoItem.description;
+			todoDueDate.textContent = todoItem.due_date;
+			todoPriority.textContent = todoItem.priority;
 
-		todoItemContainer.appendChild(todoTitle);
-		todoItemContainer.appendChild(todoDesc);
-		todoItemContainer.appendChild(todoDueDate);
-		todoItemContainer.appendChild(todoPriority);
-	})
+			todoItemContainer.appendChild(todoTitle);
+			todoItemContainer.appendChild(todoDesc);
+			todoItemContainer.appendChild(todoDueDate);
+			todoItemContainer.appendChild(todoPriority);
+		})
+	}
+	console.log("Goes Straight Here");
+	console.log(todoList.length);
 	const currentContainer = document.getElementById("current-container");
 	contentContainer.removeChild(currentContainer);
 	contentContainer.appendChild(todoItemContainer);
